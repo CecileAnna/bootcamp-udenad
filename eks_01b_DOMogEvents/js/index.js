dfaebb7724aa150aade0b8c0da844fb056ab6f4b
CecileAnna <6263325@rts-365.dk>
2021-08-17 14:26:13 +0200

    add assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,51 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+    // skriv din kode her...
+    let textCountP = document.querySelector('#text-count');
+
+    let number = textCountP.textContent;
+
+    let startNum = 8;
+
+    number = startNum;
+
+    function updateNumber() {
+
+        textCountP.textContent = number;
+
+        // if (textCountContent == NaN) {
+        //     textCountContent = parseInt(textCountContent);
+        // }
+
+    };
+
+    updateNumber();
+
+    let btnDecrease = document.querySelector('#btn-count-decrease');
+
+    let btnIncrease = document.querySelector('#btn-count-increase');
+
+    function eventHandlerIncrease(e) {
+        let maxNum = 10;
+        if (number < maxNum) {
+            number++;
+            updateNumber();
+        }
+
+    }
+
+    function eventHandlerDecrease(e) {
+        let minNum = 0;
+        if (number > minNum) {
+            number--;
+            updateNumber();
+        }
+
+    }
+
+    btnIncrease.addEventListener('click', eventHandlerIncrease);
+
+    btnDecrease.addEventListener('click', eventHandlerDecrease);
 
 }); // Afslutter: DOMContentLoaded
