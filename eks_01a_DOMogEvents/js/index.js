ad7061f50bc23119a12ff7f0f8fc8b02e163f2c3
Cecile Zimmermann <cecileaczim@gmail.com>
2021-08-17 12:58:53 +0200

    add assignment 01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -1,7 +1,47 @@
-
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+    // skriv din kode her...
+    let textCountP = document.querySelector('#text-count');
+
+    let number = textCountP.textContent;
+
+    number = 5;
+
+    function updateNumber() {
+
+        textCountP.textContent = number;
+
+        // if (textCountContent == NaN) {
+        //     textCountContent = parseInt(textCountContent);
+        // }
+
+    };
+
+    updateNumber();
+
+    let btnDecrease = document.querySelector('#btn-count-decrease');
+
+    let btnIncrease = document.querySelector('#btn-count-increase');
+
+    function eventHandlerIncrease(e) {
+        if (number < 10) {
+            number++;
+            updateNumber();
+        }
+
+    }
+
+    function eventHandlerDecrease(e) {
+        if (number > 0) {
+            number--;
+            updateNumber();
+        }
+
+    }
+
+    btnIncrease.addEventListener('click', eventHandlerIncrease);
+
+    btnDecrease.addEventListener('click', eventHandlerDecrease);
 
 }); // Afslutter: DOMContentLoaded
